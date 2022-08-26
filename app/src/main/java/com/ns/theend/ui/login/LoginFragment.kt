a0616c0e0e8ffcbde7de1f13579cd209c9215223
enesarisoy <arisoyeness@gmail.com>
2022-08-26 20:17:05 +0300

    authorization added

--- app/src/main/java/com/ns/theend/ui/login/LoginFragment.kt
+++ app/src/main/java/com/ns/theend/ui/login/LoginFragment.kt
@@ -2,7 +2,9 @@ package com.ns.theend.ui.login
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.ns.theend.R
 import com.ns.theend.databinding.FragmentLoginBinding
 import com.ns.theend.ui.BaseFragment
@@ -13,10 +15,13 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
     FragmentLoginBinding::inflate
 ) {
 
+    private var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         initClick()
+        checkUser()
 
     }
 
@@ -24,5 +29,37 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(
         binding.tvSignup.setOnClickListener {
             findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
         }
+        binding.floatingActionButton.setOnClickListener {
+            signIn()
+        }
     }
+
+    private fun checkUser() {
+        firebaseAuth.currentUser?.let {
+
+        }?: kotlin.run {
+            findNavController().navigate(R.id.action_loginFragment_to_movieFragment)
+        }
+    }
+
+    private fun signIn() {
+        val email = binding.etEmail.text.toString()
+        val pass = binding.etPassword.text.toString()
+
+        if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+            firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    findNavController().navigate(R.id.action_loginFragment_to_movieFragment)
+                } else {
+                    Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        } else {
+            Toast.makeText(context, "Empty fields are not allowed", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
 }
--- app/src/main/java/com/ns/theend/ui/register/RegisterFragment.kt
+++ app/src/main/java/com/ns/theend/ui/register/RegisterFragment.kt
@@ -2,7 +2,9 @@ package com.ns.theend.ui.register
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.ns.theend.R
 import com.ns.theend.databinding.FragmentRegisterBinding
 import com.ns.theend.ui.BaseFragment
@@ -12,8 +14,11 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(
     FragmentRegisterBinding::inflate
 ) {
 
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         initClick()
 
     }
@@ -21,6 +26,32 @@ class RegisterFragment : BaseFragment<FragmentRegisterBinding>(
     private fun initClick() {
         binding.tvSignIn.setOnClickListener {
             findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
+
+        binding.floatingActionButton.setOnClickListener {
+            signUp()
+        }
+    }
+
+    private fun signUp() {
+        val name = binding.etName.text.toString()
+        val email = binding.etEmail.text.toString()
+        val pass = binding.etPassword.text.toString()
+
+        if (name.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty()) {
+            firebaseAuth = FirebaseAuth.getInstance()
+
+            firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(context, "Successfully created!", Toast.LENGTH_SHORT).show()
+                    findNavController().navigate(R.id.action_registerFragment_to_movieFragment)
+                } else {
+                    Toast.makeText(context, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                }
+            }
+        } else {
+            Toast.makeText(context, "Empty fields are not allowed", Toast.LENGTH_SHORT).show()
         }
     }
 
